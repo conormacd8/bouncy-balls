9fbb7c0e88e556d68013afb9e14742757da1df0b
Conor MacDonald <conormacd8@gmail.com>
2022-06-28 10:52:51 +1000

    Stopped balls from spawning in the walls

--- index.js
+++ index.js
@@ -35,11 +35,11 @@ class Ball {
 let ballsArray = []
 
 for (let i = 0; i < 50; i++) {
-  let radius = (Math.random()*20)+10
-  let x = (Math.random()* (canvas.width+radius*2))+radius
-  let y = (Math.random()* (canvas.height+radius*2))+radius
-  let dx = (Math.random()-0.5)*4
-  let dy = (Math.random()-0.5)*4
+  let radius = (Math.random()*20)+30
+  let x = Math.random() * (canvas.width - radius *2) + radius
+  let y = Math.random() * (canvas.height - radius *2) + radius
+  let dx = (Math.random()-0.5)*10
+  let dy = (Math.random()-0.5)*10
   let alpha = (Math.random())
   let color = `rgba(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${alpha})`
   ballsArray.push(new Ball(x,y,dx,dy,radius,color))
